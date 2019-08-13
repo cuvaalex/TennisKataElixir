b1c8571f636ea5133347470da87420feddeb29f9
Alexandre Cuva <alexandre.cuva@finix.asia>
2019-08-13 22:18:04 +0200

    Refactor code 1

--- lib/score.ex
+++ lib/score.ex
@@ -1,27 +1,36 @@
 defmodule Score do
   @moduledoc false
 
-  def spokenScore({player1, player2})
-      when player1 == player2 and player1 >= 3 do
+  def spokenScore(player1, player2)
+    when player1 == player2 do
+      speak(player1, player2, :equal)
+  end
+
+  def spokenScore(player1, player2) do
+    speak(player1, player2, :notequal)
+  end
+
+
+  def speak(player1, player2, :equal)
+      when player1 >= 3 do
     "deuce"
   end
 
-  def spokenScore({player1, player2})
-    when player1 == player2 do
-    "#{speak(player1)} all"
+  def speak(player1, player2, :equal) do
+    "#{speakScore(player1)} all"
   end
 
 
-  def spokenScore({player1, player2})
+  def speak(player1, player2, :notequal)
     when player1 >=4 and player2 < player1 do
      "player 1 win"
   end
 
-  def spokenScore({player1, player2}) do
-    "#{speak(player1)} #{speak(player2)}"
+  def speak(player1, player2, :notequal) do
+    "#{speakScore(player1)} #{speakScore(player2)}"
   end
 
-  defp speak(player) do
+  defp speakScore(player) do
     case player do
       0 -> "love"
       1 -> "fifteen"
--- test/score_test.exs
+++ test/score_test.exs
@@ -8,39 +8,39 @@ defmodule ScoreTest do
   doctest Score
 
   test "Game with no score then return Love All" do
-    assert "love all" == Score.spokenScore({0,0})
+    assert "love all" == Score.spokenScore(0,0)
   end
 
   test "Game score 1 to 0 then return fifteen, love" do
-    assert "fifteen love" == Score.spokenScore({1, 0})
+    assert "fifteen love" == Score.spokenScore(1,0)
   end
 
   test "Game score 2 to 0 then return thirty, love" do
-    assert "thirty love" == Score.spokenScore({2, 0})
+    assert "thirty love" == Score.spokenScore(2, 0)
   end
 
   test "Game score 3 to 0 then return forty, love" do
-    assert "forty love" == Score.spokenScore({3, 0})
+    assert "forty love" == Score.spokenScore(3, 0)
   end
 
   test "Game score 4 to 0 then return player 1 win" do
-    assert "player 1 win" == Score.spokenScore({4, 0})
+    assert "player 1 win" == Score.spokenScore(4, 0)
   end
 
   test "Game score 1 to 1 then return fifteen all" do
-    assert "fifteen all" == Score.spokenScore({1,1})
+    assert "fifteen all" == Score.spokenScore(1,1)
   end
 
   test "Game score 2 to 2 then return thirty all" do
-    assert "thirty all" == Score.spokenScore({2,2})
+    assert "thirty all" == Score.spokenScore(2,2)
   end
 
   test "Game score 3 to 3 then return deuce" do
-    assert "deuce" == Score.spokenScore({3,3})
+    assert "deuce" == Score.spokenScore(3,3)
   end
 
   test "Game score 0 to 1 then return love fifteen" do
-    assert "love fifteen" == Score.spokenScore({0,1})
+    assert "love fifteen" == Score.spokenScore(0,1)
   end
 
 end
