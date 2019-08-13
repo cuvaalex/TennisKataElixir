aef8c6c2b6253995fe0225684cd0d8899c607fd1
Alexandre Cuva <alexandre.cuva@finix.asia>
2019-08-13 12:53:10 +0200

    Init project

---
+++ lib/tennis_kata_elixir.ex
@@ -0,0 +1,18 @@
+defmodule TennisKataElixir do
+  @moduledoc """
+  Documentation for TennisKataElixir.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TennisKataElixir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/tennis_kata_elixir_test.exs
@@ -0,0 +1,8 @@
+defmodule TennisKataElixirTest do
+  use ExUnit.Case
+  doctest TennisKataElixir
+
+  test "greets the world" do
+    assert TennisKataElixir.hello() == :world
+  end
+end
